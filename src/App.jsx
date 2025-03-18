c8caab50bb8db5e9575f69db9fd7750356173968
s s <75931684+suhasini-smy@users.noreply.github.com>
2025-03-18 19:37:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/HeroSection";
+
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
